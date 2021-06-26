849f2ee65bf756b8933b142649579017255b8fe1
borvel <boris.pavel37@gmail.com>
2021-06-26 19:02:39 +0300

    final touches

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Mixed Messages
-> A message generator program..
+> A workout themed message generator program.
 
 ## Table of Contents
 * [General Info](#general-information)
@@ -21,8 +21,10 @@ Every time the user runs the program, they should get a new randomized output.
 
 
 ## Features
+- Print a random workout for you.
 
-
+## Screenshots
+![Example screenshot](./img/Screenshot_1.png)
 
 ## Setup
 Install Node.js
@@ -32,32 +34,20 @@ node script.js
 ```
 
 ## Usage
-<!-- How does one go about using it?
-Provide various use cases and code examples here.
-
-`write-your-code-here` -->
+Simply run the script and a generated message will print on the terminal.
 
 
 ## Project Status
-Project is: _in progress_.
+Project is: _complete_.
 
 
 ## Room for Improvement
-<!-- Include areas you believe need improvement / could be improved. Also add TODOs for future development.
-
 Room for improvement:
-- Improvement to be done 1
-- Improvement to be done 2
-
-To do:
-- Feature to be added 1
-- Feature to be added 2 -->
-
+- Create a console to register commands such as add message, delete message to print customized messages.
 
 ## Acknowledgements
 Give credit here.
 - This project was inspired by...codecademy
-- This project was based on [this tutorial](https://www.codecademy.com/paths/full-stack-engineer-career-path/tracks/fscp-javascript-syntax-portfolio-project/modules/fscp-mixed-messages/kanban_projects/mixed-messages).
 
 
 ## Contact
--- script.js
+++ script.js
@@ -1,1 +1,61 @@
-console.log('Hi');
+const messages = {
+    _focus: ['arms', 'legs', 'chest', 'abdomen', 'upper back', 'lower back'],
+    _intended: ['flexibility', 'strength', 'speed', 'cardio', 'mobility'],
+    _intensity: ['relaxing', 'easy', 'medium', 'hard', 'master'],
+    get focus() {
+        return this._focus;
+    },
+    set focus(focus) {
+        this._focus.push(focus);
+    },
+    get intended() {
+        return this._intended;
+    },
+    set intended(intended) {
+        this._intended.push(intended);
+    },
+    get intensity() {
+        return this._intensity;
+    },
+    set intensity(intensity) {
+        this._intensity.push(intensity);
+    },
+    dropFocus(element) {
+        if (element === undefined) {
+            this._focus.pop();
+            return;
+        }
+        let index = this._focus.indexOf(element);
+        if (index === -1)
+            return;
+        this._focus.splice(index, 1);
+    },
+    dropIntended(element) {
+        if (element === undefined) {
+            this._intended.pop();
+            return;
+        }
+        let index = this._intended.indexOf(element);
+        if (index === -1)
+            return;
+        this._intended.splice(index, 1);
+    },
+    dropIntensity(element) {
+        if (element === undefined) {
+            this._intensity.pop();
+            return;
+        }
+        if (index === -1)
+            return;
+        let index = this._intensity.indexOf(element);
+        this._intensity.splice(index, 1);
+    },
+    randomlySelect(arr) {
+        return arr[Math.floor(Math.random() * arr.length)];
+    },
+    printRadomMessage() {
+        console.log(`Today, you will focus on ${this.randomlySelect(this._focus)} \nYou will train for ${this.randomlySelect(this._intended)} \nAnd have a ${this.randomlySelect(this._intensity)} workout`);
+    }
+
+};
+messages.printRadomMessage();
